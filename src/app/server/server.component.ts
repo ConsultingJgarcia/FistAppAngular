162bef697a982396f8053b70837f55747228e751
Josvany_G <josvanyg@gmail.com>
2022-12-14 10:59:40 -0600

    basic

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,12 +1,14 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html',
-    // styleUrls: ['./app.component.css']
+  selector: 'app-server',
+  templateUrl: './server.component.html'
 })
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
-export class ServerComponent
-{
-
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
